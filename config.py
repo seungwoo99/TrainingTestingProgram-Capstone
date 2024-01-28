9256da349aa6051b02626c67e6bf1bd4571d8d12
seungwoo99 <114711192+seungwoo99@users.noreply.github.com>
2024-01-28 14:08:13 -0800

    Verification Process

---
+++ config.py
@@ -0,0 +1,10 @@
+#config.py
+#stores admin user's email information to send verification email
+
+class MailConfig:
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = 'TrainingTestingProgram@gmail.com'
+    MAIL_PASSWORD = 'ofzn elsi doxw lrun'
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
--- devapp.py
+++ devapp.py
@@ -5,19 +5,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import func, text
 from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv, dotenv_values  # pip install python-dotenv
+from flask_mail import Message, Mail
+from random import randint
+from datetime import datetime, timezone, timedelta
+from config import MailConfig
 
 load_dotenv()
 
 app = Flask(__name__)
 
 # Configure Flask app to use SQLAlchemy for a local MySQL database
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/test_train_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost:3306/test_train_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+# Configure Flask app to use Mail system
+app.config.from_object(MailConfig)
 
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+# Initialize Mail
+mail = Mail(app)
 
 
 # Hash the password before storing it in the database
@@ -42,27 +50,8 @@ def trylogin():
     messages = get_flashed_messages()
 
     # Serve login page
-    return render_template("login.html", messages=messages)
+    return render_template("home.html", messages=messages)
 
-
-@app.route("/tryregister")
-def tryregister():
-
-    # Check if user session is inactive
-    if 'user' not in session or not session['user'].get('is_authenticated', False):
-        flash("Access denied, please login.")
-        return redirect(url_for('trylogin'))
-
-    # Check if user is not an admin
-    if session['user']['is_admin'] == 0:
-        flash("Access denied, page is admin only.")
-        return redirect(url_for('homepage'))
-
-    # Check for any failed login or access denied messages
-    messages = get_flashed_messages()
-
-    # Serve login page
-    return render_template("register.html", messages=messages)
 
 @app.route("/homepage")
 def homepage():
@@ -102,87 +91,73 @@ def login():
                 'is_admin': row['is_admin'], 'is_authenticated': False}
 
         # Check if entered password matches hashed_password
-        if bcrypt.check_password_hash(hashed_password, input_password):
+        #if bcrypt.check_password_hash(hashed_password, input_password):
+        if input_password == hashed_password:
+            # Create otp
+            session[f'email_{input_username}'] = user['email']
+            otp_created_time = datetime.now(timezone.utc)
+            otp = generate_otp(user['email'], otp_created_time)
 
-            # Check if account is verified
-            if user['is_verified'] == 0:
 
-                # Flash failed authentication message and redirect to login page
-                flash("Account not verified, please check your email")
-                return redirect(url_for('trylogin'))
+            # Send email to user using SMTP - Simple Mail Transfer Protocol
+            msg = Message('Training Test Program Verification.', sender=app.config['MAIL_USERNAME'],
+                          recipients=[user['email']])
+            msg.body = ('Dear ' + user['name'] +
+                        '\n\nWe received a request to access your account ' + user['username'] + '.' +
+                        '\nPlease insert verification code.\nVerification code: '+str(otp))
+            mail.send(msg)
 
             # Set up user session cookie
             session['user'] = user
             session['user']['is_authenticated'] = True
 
             # Redirect to homepage
-            return redirect(url_for('homepage'))
+            return render_template('verification.html', password=input_password, time=otp_created_time, user_email=user['email'])
         else:
             # Flash failed authentication message and redirect to login page
             flash("Invalid username or password")
             return redirect(url_for('trylogin'))
 
 
-@app.route("/register", methods=["POST"])
-def register():
-
-    if request.method == "POST":
-
-        # Load form variables
-        input_username = request.form.get("username")
-        input_password = request.form.get("password")
-        input_confirm_password = request.form.get("confirm_password")
-        input_email = request.form.get("email")
-        input_first_name = request.form.get("first_name")
-        input_last_name = request.form.get("last_name")
-        input_is_admin = request.form.get("is_admin")
-
-        # Check if passwords match
-        if input_password != input_confirm_password:
-            # Flash failed authentication message and redirect to register page
-            flash("Entered passwords do not match")
-            return redirect(url_for('tryregister'))
-
-        # Check if by username or email is already registered
-        query = text("SELECT * FROM user WHERE username = :username OR email = :email")
-        result = db.engine.execute(query, username=input_username, email=input_email)
-
-        row = result.fetchone()
-
-        # Check if a row was found
-        if row:
-            # Determine if the input corresponds to the found username or email
-            if input_username == row['username']:
-                flash("Username " + input_username + " already exists")
-                return redirect(url_for('tryregister'))
-            else:
-                flash("Email " + input_email + " already exists")
-                return redirect(url_for('tryregister'))
+# Generate a verification otp
+def generate_otp(email, time):
+    otp = str(randint(100000, 999999))
+    session[f'otp_{email}'] = otp
+    session[f'time_{email}'] = time
+    return otp
 
 
-        else:
-            # Insert the user into the database
-            insert_query = text("""
-                INSERT INTO user (username, email, password, name, is_admin, is_verified)
-                VALUES (:username, :email, :password, :name, :is_admin, :is_verified)
-            """)
+#action when submit button is clicked
+@app.route('/verification', methods=['POST'])
+def verify_otp():
+    otp = request.form['otp']
+    session_email = request.form['user_email']
 
-            # Assuming you have a function to hash passwords (e.g., hash_password)
-            hashed_password = bcrypt.generate_password_hash(input_password)
+    session_otp = session.get(f'otp_{session_email}')
+    session_time = session.get(f'time_{session_email}')
 
-            if input_is_admin == "on":
-                is_admin = 1
-            else:
-                is_admin = 0
+    # When otp and time is not stored in session
+    if not session_otp or not session_time:
+        verification_result = {'category': 'error', 'message': 'Invalid OTP. Please try again.'}
+        return render_template('verification.html',verification_result=verification_result, user_email=session_email)
 
-            # Execute insert statement
-            db.engine.execute(insert_query, username=input_username, email=input_email,
-                              password=hashed_password, name=f"{input_first_name} {input_last_name}",
-                              is_admin=is_admin, is_verified=0)
+    # When session is over
+    current_time = datetime.now(timezone.utc)
+    if current_time > session_time + timedelta(minutes=5):
+        # Delete otp and time in session
+        session.pop(f'otp_{session_email}')
+        session.pop(f'time_{session_email}')
+        verification_result = {'category': 'error', 'message': 'OTP verification is expired. Please try again.'}
+        return render_template('verification.html', verification_result=verification_result, user_email=session_email)
 
-            # Return to homepage
-            return redirect(url_for("homepage"))
+    if otp == session_otp:  # Success in verification
+        session.pop(f'otp_{session_email}')
+        session.pop(f'time_{session_email}')
+        return "<h2>This is the under-construction homepage</h2><a href=\"/logout\">Logout</a>"
+    elif otp != session_otp:
+        verification_result = {'category': 'error', 'message': 'Invalid OTP. Please try again.'}
+        return render_template('verification.html', verification_result=verification_result, user_email=session_email)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5003)
